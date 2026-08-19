b4b70ea5ce3af7e3c00393af8f7c845420d671ac
SebMut <sebastian.mutter@outlook.com>
2026-08-19 14:23:58 +0200

    PWA Cache auf V4 aktualisieren

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
-const CACHE='angebotsradar-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./data/offers-live.json'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+const CACHE='angebotsradar-v4.1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
+  if(event.request.url.includes('/data/offers-live.json')){
+    event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request,{ignoreSearch:true})));
+    return;
+  }
   event.respondWith(fetch(event.request).then(response=>{
     const clone=response.clone();
     caches.open(CACHE).then(cache=>cache.put(event.request,clone));
