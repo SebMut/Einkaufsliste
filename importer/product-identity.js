fd2262a00974ee94a2d18e76f7a228558c989747
SebMut <sebastian.mutter@outlook.com>
2026-08-19 23:39:28 +0200

    Produktidentität auf zentrale GTIN-Prüfung umstellen

--- importer/product-identity.js
+++ importer/product-identity.js
@@ -1,14 +1,8 @@
 import { slug, norm } from './product-normalizer.js';
 import { buildConcreteIdentityParts } from './semantic-products.js';
+import { normalizeGtin } from './gtin.js';
 
-export function normalizeGtin(value = '') {
-  const digits = String(value ?? '').replace(/\D/g, '');
-  if (![8, 12, 13, 14].includes(digits.length)) return null;
-  let sum = 0;
-  for (let i = digits.length - 2, pos = 0; i >= 0; i--, pos++) sum += Number(digits[i]) * (pos % 2 === 0 ? 3 : 1);
-  const check = (10 - (sum % 10)) % 10;
-  return check === Number(digits.at(-1)) ? digits : null;
-}
+export { normalizeGtin } from './gtin.js';
 
 function productLabel(input = {}) {
   const brand = norm(input.brand);
